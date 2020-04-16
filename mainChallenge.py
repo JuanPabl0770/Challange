0a0b64db53ad82812daa5ed3bde868326567c0a3
JuanPabl0770 <53346852+JuanPabl0770@users.noreply.github.com>
2020-04-16 11:21:24 -0500

    Second function implemented

--- mainChallenge.py
+++ mainChallenge.py
@@ -22,8 +22,27 @@ def calculate_Ngrams(text, N):
 
 def mostFreqentNgram(text,N):
     ngrams = calculate_Ngrams(text,N)
+    ngram_reps = []
+    cantMostFrequent = 0;
+    posMostFrequent = -1;
 
+    for i in range (len(ngrams)):
+        for j in ngram_reps:
+            if j[0] == ngrams[i]:
+                j[1] += 1
+                if j[1] > cantMostFrequent:
+                    cantMostFrequent = j[1]
+                    posMostFrequent = i
+                break
+        #endfor
+        ngram_reps.append([ngrams[i],1])
+    #end for
+    print(ngrams)
+    print(ngram_reps)
+    return [ngrams[posMostFrequent]]
+#end def
 
 word = "to be or not to be"
 print(calculate_Ngrams(word,2))
+print(mostFreqentNgram(word,2))
 
